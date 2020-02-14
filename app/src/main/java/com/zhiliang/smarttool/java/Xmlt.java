ac925b2257eef76d26cb1fd5a1a0ce3e771cbf50
YZL19 <zhiliang_2014@163.com>
2020-02-15 00:30:37 +0800

    xml翻译文件改为写入,添加POI依赖

---
+++ app/src/main/java/com/zhiliang/smarttool/java/Xmlt.java
@@ -0,0 +1,10 @@
+package com.zhiliang.smarttool.java;
+
+import java.util.ArrayList;
+
+public class Xmlt {
+    public String id;
+    public String type;
+    public String mString;
+    public ArrayList<String> mStrings;
+}
---
+++ poi/src/main/java/com/zhiliang/poi/MyPoiClass.java
@@ -0,0 +1,31 @@
+package com.zhiliang.poi;
+
+public class MyPoiClass {
+    /*public static void main(String[] args) {
+        File xlsFile = new File("poi.xls");
+        // 获得工作簿
+        Workbook workbook = WorkbookFactory.create(xlsFile);
+        // 获得工作表个数
+        int sheetCount = workbook.getNumberOfSheets();
+        // 遍历工作表
+        for (int i = 0; i < sheetCount; i++) {
+            Sheet sheet = workbook.getSheetAt(i);
+            // 获得行数
+            int rows = sheet.getLastRowNum() + 1;
+            // 获得列数，先获得一行，在得到改行列数
+            Keyboard.Row tmp = sheet.getRow(0);
+            if (tmp == null) {
+                continue;
+            }
+            int cols = tmp.getPhysicalNumberOfCells();
+            // 读取数据
+            for (int row = 0; row < rows; row++) {
+                Keyboard.Row r = sheet.getRow(row);
+                for (int col = 0; col < cols; col++) {
+                    System.out.printf("%10s", r.getCell(col).getStringCellValue());
+                }
+                System.out.println();
+            }
+        }
+    }*/
+}
